4757e3045935ea20869f6b256132409dd1058d10
Afoiu <bgc.anast@gmail.com>
2020-10-29 12:06:01 +0200

    Merging changes from Branch1 and updated master

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    public void PrintSting (String txt) {
+        System.out.println(txt);
+    }
+
     public static void sum (int...a){
         int sum=0;
         for (int i : a ) {
@@ -9,5 +13,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Cnanges on Branch 2");
         sum(5,7,3);
+        Main test = new Main();
+        test.PrintSting("Changes on branch 1");
     }
 }
